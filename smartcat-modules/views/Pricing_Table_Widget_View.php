77e2032361000b5c8dda6dca60f1c1045f1a0605
Zack Mitchell <zackcmitchell@gmail.com>
2017-04-05 14:36:20 -0400

    Updated Pricing Table widget view

--- smartcat-modules/views/Pricing_Table_Widget_View.php
+++ smartcat-modules/views/Pricing_Table_Widget_View.php
@@ -1,4 +1,4 @@
-<div class="juno-pricing-table <?php echo isset( $instance['scmod_pricing_table_width'] ) ? 'col-sm-' . $instance['scmod_pricing_table_width'] : 'col-sm-4'; ?>">
+<div class="byblos-pricing-table <?php echo isset( $instance['scmod_pricing_table_width'] ) ? 'col-sm-' . $instance['scmod_pricing_table_width'] : 'col-sm-4'; ?>">
     
     <div class="widget">
         
@@ -8,10 +8,12 @@
                 <span class="special"><span class="fa fa-star"></span></span>
             <?php endif; ?>
 
-            <h2 class="title">
+            <h2 class="widget-title">
                 <?php echo !empty( $instance['scmod_pricing_table_title'] ) ? esc_html( $instance['scmod_pricing_table_title'] ) : ''; ?>
             </h2>
 
+            <div class="byblos-underline"></div>
+                
             <div class="price">
                 <?php echo !empty( $instance['scmod_pricing_table_price'] ) ? esc_html( $instance['scmod_pricing_table_price'] ) : ''; ?>
             </div>
@@ -25,7 +27,7 @@
             </div>
 
             <?php if ( !empty( $instance['scmod_pricing_table_url'] ) && !empty( $instance['scmod_pricing_table_button_label'] ) ) : ?>
-                <a class="accent-button" href="<?php echo esc_url( $instance['scmod_pricing_table_url'] ); ?>">
+                <a class="button button-primary" href="<?php echo esc_url( $instance['scmod_pricing_table_url'] ); ?>">
                     <?php echo esc_html( $instance['scmod_pricing_table_button_label'] ); ?>
                 </a>
             <?php endif;?>
