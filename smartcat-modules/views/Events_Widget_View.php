400cc29777c5193736529aa8d007797a7c7e80c5
Zack Mitchell <zackcmitchell@gmail.com>
2017-04-06 16:42:16 -0400

    More adjustments to SC Modules widget designs

--- smartcat-modules/views/Events_Widget_View.php
+++ smartcat-modules/views/Events_Widget_View.php
@@ -19,83 +19,85 @@ $args = array (
 );
 
 // The Query
-$events = new WP_Query( $args );
+$events = new WP_Query( $args ); ?>
 
-// The Loop
-if ( $events->have_posts() ) : ?>
-
-    <div class="juno-events <?php echo isset( $instance['scmod_events_widget_width'] ) ? 'col-sm-' . $instance['scmod_events_widget_width'] : 'col-sm-12'; ?>">
+    <div class="byblos-events <?php echo isset( $instance['scmod_events_widget_width'] ) ? 'col-sm-' . $instance['scmod_events_widget_width'] : 'col-sm-12'; ?>">
 
         <div class="widget">
         
-            <h2 class="widget-title cpt-widget-title">
-                <?php echo !empty( $instance['scmod_events_title'] ) ? esc_html( $instance['scmod_events_title'] ) : ''; ?>
-            </h2>
+            <div class="inner">
+            
+                <h2 class="widget-title cpt-widget-title">
+                    <?php echo !empty( $instance['scmod_events_title'] ) ? esc_html( $instance['scmod_events_title'] ) : ''; ?>
+                </h2>
 
-            <div class="row">
+                <div class="byblos-underline"></div>
+                
+                <?php if ( $events->have_posts() ) : ?>
 
-                <?php $ctr = 0; ?>
+                    <?php $ctr = 0; ?>
 
-                <?php while ( $events->have_posts() ) : ?>
+                    <?php while ( $events->have_posts() ) : ?>
 
-                    <?php $ctr++; ?>
+                        <?php $ctr++; ?>
 
-                    <?php $events->the_post(); ?>
+                        <?php $events->the_post(); ?>
 
-                    <div class="col-sm-6">
+                        <div class="col-sm-6">
 
-                        <div class="event-post">
+                            <div class="event-post">
 
-                            <?php if ( has_post_thumbnail() ) : ?>
+                                <?php if ( has_post_thumbnail() ) : ?>
 
-                                <a href="<?php echo esc_url( get_the_permalink( get_the_ID() ) ); ?>">
-                                    <?php the_post_thumbnail('large'); ?>
-                                </a>
-
-                            <?php endif; ?>
-
-                            <div class="event-details">
-
-                                <div class="location">
-                                    <?php echo get_post_meta( get_the_ID(), 'event_meta_location', true ); ?>
-                                </div>
-                                
-                                <h2 class="title">
-
-                                    <a target="_BLANK" href="<?php echo esc_url( get_the_permalink( get_the_ID() ) ); ?>">
-                                        <?php echo the_title(); ?>
+                                    <a class="image-link" href="<?php echo esc_url( get_the_permalink( get_the_ID() ) ); ?>">
+                                        <?php the_post_thumbnail('large'); ?>
                                     </a>
 
-                                </h2>
+                                <?php endif; ?>
 
-                                <div class="date">
+                                <div class="event-details">
 
-                                    <?php echo date( 'M jS, Y', strtotime( get_post_meta( get_the_ID(), 'event_meta_date', true ) ) ); ?>
+                                    <h2 class="title">
+                                        <a target="_BLANK" href="<?php echo esc_url( get_the_permalink( get_the_ID() ) ); ?>">
+                                            <?php echo the_title(); ?>
+                                        </a>
+                                    </h2>
 
-                                    <?php echo date( 'g:i', strtotime( get_post_meta( get_the_ID(), 'event_meta_time_start', true ) ) ); ?>
-                                    to <?php echo date( 'g:i a', strtotime( get_post_meta( get_the_ID(), 'event_meta_time_end', true ) ) ); ?>
+                                    <div class="location">
+                                        <?php echo get_post_meta( get_the_ID(), 'event_meta_location', true ); ?>
+                                    </div>
+
+                                    <div class="date">
+
+                                        <?php echo date( 'M jS, Y', strtotime( get_post_meta( get_the_ID(), 'event_meta_date', true ) ) ); ?>
+
+                                        <?php echo date( 'g:i', strtotime( get_post_meta( get_the_ID(), 'event_meta_time_start', true ) ) ); ?>
+                                        to <?php echo date( 'g:i a', strtotime( get_post_meta( get_the_ID(), 'event_meta_time_end', true ) ) ); ?>
+
+                                    </div>
+
+                                    <div class="clear"></div>
 
                                 </div>
-
-                                <div class="clear"></div>
 
                             </div>
 
                         </div>
 
-                    </div>
+                    <?php endwhile; ?>
+            
+                    <div class="clear"></div>
+                
+                <?php else : ?>
 
-                <?php endwhile; ?>
+                    <h4 class="none-to-display"><?php _e( 'There are currently no upcoming events, please check again at a later time.', 'byblos' ); ?></h4>
 
+                <?php endif; ?>
+                
             </div>
+            
         </div>
 
     </div>
-
-<?php else : ?>
-
-    <h4 class="none-to-display"><?php _e( 'There are currently no upcoming events, please check again at a later time.', 'juno' ); ?></h4>
-
-<?php endif; ?>
 
 <?php wp_reset_postdata(); ?>
--- smartcat-modules/views/Gallery_Widget_View.php
+++ smartcat-modules/views/Gallery_Widget_View.php
@@ -14,7 +14,7 @@ $gallery_style = empty( $instance['scmod_gallery_shuffle'] ) || $instance['scmod
 <script type="text/javascript">
 
     jQuery(document).ready(function(){
-        jQuery(".juno-gallery-contents").unitegallery({
+        jQuery(".byblos-gallery-contents").unitegallery({
             <?php if ( !empty( $instance['scmod_gallery_tile_style'] ) && $instance['scmod_gallery_tile_style'] != 'columns' ) : ?>
                 tiles_type: '<?php echo $instance['scmod_gallery_tile_style'] == 'justified' ? 'justified' : 'nested'; ?>',
             <?php endif; ?>
@@ -22,58 +22,58 @@ $gallery_style = empty( $instance['scmod_gallery_shuffle'] ) || $instance['scmod
             lightbox_type: 'compact',
             tile_enable_icons: false,
             theme_appearance_order: '<?php echo $gallery_style; ?>',
-            tiles_space_between_cols: 5,
+            tiles_space_between_cols: <?php echo esc_js( get_theme_mod( 'byblos_gallery_gutter_space', 5 ) ); ?>,
+            tiles_space_between_cols_mobile: <?php echo esc_js( get_theme_mod( 'byblos_gallery_gutter_space', 5 ) ); ?>,
+            tiles_justified_space_between: <?php echo esc_js( get_theme_mod( 'byblos_gallery_gutter_space', 5 ) ); ?>,
             lightbox_slider_image_border: false,
             lightbox_slider_image_shadow: false,
             lightbox_overlay_opacity: .95,
-            tile_overlay_opacity: 0.75,
-            tile_overlay_color: "<?php echo esc_js( get_theme_mod( 'juno_theme_color_primary', '#72c4c0' ) ); ?>",
+            tile_overlay_opacity: <?php echo esc_js( get_theme_mod( 'byblos_gallery_hover_opacity', .33 ) ); ?>,
+            tile_overlay_color: "<?php echo esc_js( get_theme_mod( 'byblos_gallery_hover_color', '#ffffff' ) ); ?>",
         });
     });
 
 </script>
 
-<div class="juno-gallery <?php echo isset( $instance['scmod_gallery_widget_width'] ) ? 'col-sm-' . $instance['scmod_gallery_widget_width'] : 'col-sm-12'; ?>">
+<div class="byblos-gallery <?php echo isset( $instance['scmod_gallery_widget_width'] ) ? 'col-sm-' . $instance['scmod_gallery_widget_width'] : 'col-sm-12'; ?>">
 
     <div class="widget">
     
-        <h2 class="widget-title cpt-widget-title">
-            <?php echo !empty( $instance['scmod_gallery_title'] ) ? esc_html( $instance['scmod_gallery_title'] ) : ''; ?>
-        </h2>
+        <div class="inner">
+        
+            <h2 class="widget-title cpt-widget-title">
+                <?php echo !empty( $instance['scmod_gallery_title'] ) ? esc_html( $instance['scmod_gallery_title'] ) : ''; ?>
+            </h2>
 
-        <div class="row">
+            <div class="byblos-underline"></div>
 
-            <div class="col-sm-12">
+            <?php if ( !empty( $gallery ) ) : ?>
 
-                <?php if ( !empty( $gallery ) ) : ?>
+                <div id="gallery" style="display:none;" class="byblos-gallery-contents">
 
-                    <div id="gallery" style="display:none;" class="juno-gallery-contents">
+                    <?php foreach( $gallery as $item ) : ?>
 
-                        <?php foreach( $gallery as $item ) : ?>
+                        <?php $feat_image = get_the_post_thumbnail_url( $item['ID'] ); ?>
 
-                            <?php $feat_image = get_the_post_thumbnail_url( $item['ID'] ); ?>
+                        <a href="#">
+                            <img alt="<?php echo esc_attr( $item['post_title'] ); ?>"
+                                 src="<?php echo esc_url( $feat_image ); ?>"
+                                 data-image="<?php echo esc_url( $feat_image ); ?>"
+                                 data-description="<?php echo esc_attr( $item['post_title'] ); ?>">
+                        </a>
 
-                            <a href="#">
-                                <img alt="<?php echo esc_attr( $item['post_title'] ); ?>"
-                                     src="<?php echo esc_url( $feat_image ); ?>"
-                                     data-image="<?php echo esc_url( $feat_image ); ?>"
-                                     data-description="<?php echo esc_attr( $item['post_title'] ); ?>">
-                            </a>
+                    <?php endforeach; ?>
 
-                        <?php endforeach; ?>
+                    <?php wp_reset_postdata(); ?>
 
-                        <?php wp_reset_postdata(); ?>
+                </div>
 
-                    </div>
+            <?php else : ?>
 
-                <?php else : ?>
+                <h4 class="none-to-display"><?php _e( 'There are currently no photos to display in the Gallery, please check again at a later time.', 'byblos' ); ?></h4>
 
-                    <h4 class="none-to-display"><?php _e( 'There are currently no photos to display in the Gallery, please check again at a later time.', 'juno' ); ?></h4>
-
-                <?php endif; ?>
-
-            </div>
-
+            <?php endif; ?>
+        
         </div>
         
     </div>
